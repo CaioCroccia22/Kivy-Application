bd3374a0c036539aa80b2e448a36cb5534b96347
CaioCroccia22 <caiocroccia22@gmail.com>
2024-03-27 20:31:59 -0300

    button

---
+++ kivy_project/kivy_app_button.py
@@ -0,0 +1,8 @@
+import random 
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class HboxLayoutExample(App):
+    def build(self):
+        
